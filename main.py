3c114ec73d33491502619c7ae2f62e2b939631ee
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-04-20 04:17:07 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import *
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import request, current_app
+from webapp import create_app
+from time import strftime
+import traceback
+
+app = create_app()
+handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
+logger = logging.getLogger('tdm')
+logger.setLevel(logging.ERROR)
+logger.addHandler(handler)
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    tb = traceback.format_exc()
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, tb)
+    return e.status_code
+
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5002)
+
+
+if __name__ == "__main__":
+   main()
