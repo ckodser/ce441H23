0a47819f9e79b226546bf4dffdf03905cb2afcfe
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-04-20 15:54:48 +0430

    update exp

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def exceptions(e):
     return jsonify(error=str(e)), 500
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=80)
 
 
 if __name__ == "__main__":
