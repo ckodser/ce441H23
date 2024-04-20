c32bbb61a745a53cbc98954fc1dfae4f82c10114
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-04-20 13:50:39 +0430

    favicon

--- webapp/views.py
+++ webapp/views.py
@@ -7,6 +7,6 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
-@app.route('/favicon.ico')
+@views.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
