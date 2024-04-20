35f44aa818c23fadd4fc2321578b824ac77ff8b9
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-04-20 14:34:59 +0430

    add exp

--- webapp/views.py
+++ webapp/views.py
@@ -7,6 +7,10 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
+@views.route("/ce441")
+def home():
+    return render_template("exp.html")
+
 @views.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join("webapp",'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
