ace6ed73e88b9d51feec339f6eaad29a72527cf5
arshia.soltani2 <arshia.soltani2@gmail.com>
2024-04-20 13:52:32 +0430

    favicon

--- webapp/views.py
+++ webapp/views.py
@@ -9,4 +9,4 @@ def home():
 
 @views.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join("webapp",'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
